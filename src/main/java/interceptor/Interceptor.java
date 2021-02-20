a8856b7aa949aa52d15422c4a301398227be0554
윤용운_Backend <yuyun0124@gmail.com>
2021-02-21 02:32:39 +0900

    Set Spring Authorization

--- src/main/java/interceptor/Interceptor.java
+++ src/main/java/interceptor/Interceptor.java
@@ -15,17 +15,20 @@ public class Interceptor extends HandlerInterceptorAdapter {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String token = request.getHeader("Authorization");
         if ( token == null || ("".equals(token) && token.isEmpty()) ) { // 토큰이 없는경우
+            System.out.println("No Token");
             throw new Exception();
         }
         if ( token.length() < 10 ){
+            System.out.println("To Short");
             throw new Exception();
         }
-
         else if  (!token.startsWith("Bearer ")){ // 토큰이 Bearer 인증타입을 지키지않고 전송이 될때
+            System.out.println("Not Bearer");
             throw new Exception();
         }
         else {
             if ( jwtUtil.checkToken(token) ) {
+                System.out.println("is Expired");
                 throw new Exception();
             }
             return true;
