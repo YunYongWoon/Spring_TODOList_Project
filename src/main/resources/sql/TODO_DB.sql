16367309111cd74e527836541236f111f71405c8
윤용운_Backend <yuyun0124@gmail.com>
2021-02-25 22:39:41 +0900

    sql문 수정

--- src/main/resources/sql/TODO_DB.sql
+++ src/main/resources/sql/TODO_DB.sql
@@ -1,7 +1,13 @@
 create table TODO_Lists(
-                        idx bigint not null auto_increment primary key,
-                        Type Enum('TODO','Progress','Done'),
-                        TODO varchar(200) not null,
-                        User_ID bigint not null,
-                        created_at TIMESTAMP default CURRENT_TIMESTAMP,
-                        updated_at TIMESTAMP default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP);
+                           id bigint not null auto_increment primary key,
+                           TodoType Enum('TODO','Progress','Done'),
+                           Todo varchar(200) not null,
+                           user_ID bigint not null,
+                           scheduled_at TIMESTAMP,
+                           created_at TIMESTAMP default CURRENT_TIMESTAMP,
+                           updated_at TIMESTAMP default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+                           is_deleted boolean not null default 0,
+                           is_finished boolean not null default 0,
+                           is_archieved boolean not null default 0);
+
+ㅂ
--- src/main/resources/sql/User_DB.sql
+++ src/main/resources/sql/User_DB.sql
@@ -1,6 +1,6 @@
 create table Users(
-                        User_ID bigint not null auto_increment primary key,
-                        User_AccountID varchar(15) not null unique,
-                        User_Password varchar(20) not null,
-                        created_at TIMESTAMP default CURRENT_TIMESTAMP,
-                        updated_at TIMESTAMP default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP);
+                      ID bigint not null auto_increment primary key,
+                      AccountID varchar(16) not null unique,
+                      Password varchar(100) not null,
+                      created_at TIMESTAMP default CURRENT_TIMESTAMP,
+                      updated_at TIMESTAMP default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP);
