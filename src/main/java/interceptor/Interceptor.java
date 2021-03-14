299fcb3000ef35eee38be4c003631f7dde54a970
윤용운_Backend <yuyun0124@gmail.com>
2021-03-14 19:16:27 +0900

    예외처리 : Interceptor

--- src/main/java/interceptor/Interceptor.java
+++ src/main/java/interceptor/Interceptor.java
@@ -15,21 +15,17 @@ public class Interceptor extends HandlerInterceptorAdapter {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String token = request.getHeader("Authorization");
         if ( token == null || ("".equals(token) && token.isEmpty()) ) { // 토큰이 없는경우
-            System.out.println("No Token");
-            throw new Exception();
+            throw new RuntimeException("No Token");
         }
         if ( token.length() < 10 ){
-            System.out.println("To Short");
-            throw new Exception();
+            throw new RuntimeException("To Short");
         }
         else if  (!token.startsWith("Bearer ")){ // 토큰이 Bearer 인증타입을 지키지않고 전송이 될때
-            System.out.println("Not Bearer");
-            throw new Exception();
+            throw new RuntimeException("Not Bearer");
         }
         else {
             if ( jwtUtil.checkToken(token) ) {
-                System.out.println("is Expired");
-                throw new Exception();
+                throw new RuntimeException("is Expired");
             }
             return true;
         }
