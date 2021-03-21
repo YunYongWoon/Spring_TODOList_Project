8b263bf1ed578b69739cd9e40840c48731d6dabc
윤용운_Backend <yuyun0124@gmail.com>
2021-03-21 19:50:01 +0900

    페이지네이션 수정 3

--- src/main/java/service/ArchiveServiceImpl.java
+++ src/main/java/service/ArchiveServiceImpl.java
@@ -20,15 +20,17 @@ public class ArchiveServiceImpl implements ArchiveService {
     public List<ArchiveList> ReadList(Long currentPage) {
         if(currentPage == null)
             throw new RuntimeException("currentPage 값이 없습니다");
-        jwtUtil.getIdByToken();
+
+        Long id = jwtUtil.getIdByToken();
+
         Paging paging = new Paging();
-        paging.setTotalListCnt(getTotalCnt(jwtUtil.getIdByToken()));
+        paging.setTotalListCnt(getTotalCnt(id));
 
         if(currentPage < 1 || currentPage > paging.getTotalPageCnt())
             throw new RuntimeException("잘못된 페이지 정보입니다.");
 
         paging.setStartList(currentPage);
-        paging.setUser_ID(jwtUtil.getIdByToken());
+        paging.setUser_ID(id);
 
         return archiveMapper.Read(paging);
     }
